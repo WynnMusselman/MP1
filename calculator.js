7ec7ecfcf2db8bda59243ec3b7f53c2783209a9f
Wynn Musselman <wynnmuss@bu.edu>
2026-09-14 16:08:18 -0400

    fixed pow and clear function

--- calculator.js
+++ calculator.js
@@ -1,33 +1,48 @@
-let num1 = document.getElementById("first").value;
-let num2 = document.getElementById("second").value;
+let num1;
+let num2;
 let output = document.getElementById("output");
 
+function createNums(){
+    num1 = document.getElementById("first").value;
+    num2 = document.getElementById("second").value;
+}
+
 function doAdd(){
+    createNums();
     output.innerHTML = String(Number(num1) + Number(num2));
 }
 
 function doSub(){
+    createNums();
     output.innerHTML = String(Number(num1) - Number(num2));
 }
 
 
 function doMul(){
+    createNums();
     output.innerHTML = String(Number(num1) * Number(num2));
 }
 
 
 function doDiv(){
+    createNums();
     output.innerHTML = String(Number(num1) / Number(num2));
 }
 
 function doPow(){
+    createNums();
+    let output_val = 1;
+
     for(let i = 0; i < num2; i++){
-        num1 = Number(num1) * Number(num1)
+         output_val = Number(num1) * Number(output_val)
     }
-    output.innerHTML = String(num1);
+    output.innerHTML = String(output_val);
 }
 
 function doClear(){
-    document.getElementById("first").innerHTML = "";
-    document.getElementById("second").innerHTML = "";
+    num1 = document.getElementById("first");
+    num2 = document.getElementById("second");
+    num1.value = "";
+    num2.value = "";
+    output.innerHTML = "";
 }
